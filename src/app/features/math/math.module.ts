57b8da9b0cc3403923a097464e68a0c8f4e1a7f1
michaelhsladetrng <51245469+michaelhsladetrng@users.noreply.github.com>
2019-07-10 14:53:12 +0000

    Added new math component

---
+++ src/app/features/math/math.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MathComponent } from './math.component';
+
+
+
+@NgModule({
+  declarations: [MathComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class MathModule { }
