8f647b5f8d0d34a3e9690cf2ea3d48675ad54392
michaelhsladetrng <51245469+michaelhsladetrng@users.noreply.github.com>
2019-07-10 15:30:55 +0000

    Added Math Game, Scores cmps /w children routing

--- src/app/features/math/math.module.ts
+++ src/app/features/math/math.module.ts
@@ -2,16 +2,31 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MathComponent } from './math.component';
 import { Routes, RouterModule } from '@angular/router';
+import { GameComponent } from './components/game/game.component';
+import { ScoresComponent } from './components/scores/scores.component';
 
 const routes: Routes = [
   {
     path: 'math',
-    component: MathComponent
+    component: MathComponent,
+    children: [
+      {
+        path: '', redirectTo: 'game', pathMatch: 'full'
+      },
+      {
+        path: 'game',
+        component: GameComponent
+      },
+      {
+        path: 'scores',
+        component: ScoresComponent
+      }
+    ]
   }
 ];
 
 @NgModule({
-  declarations: [MathComponent],
+  declarations: [MathComponent, GameComponent, ScoresComponent],
   imports: [
     CommonModule,
     RouterModule.forChild(routes)
